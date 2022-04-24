f68cbf3f7ba142e3bef03451dd4ef2ad9e47e203
Kacper <thestahan@gmail.com>
2022-04-24 10:39:12 +0200

    Add initial project structure

---
+++ VerticalSliceArchExample/Data/ApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VerticalSliceArchExample.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+    {
+    }
+}
---
+++ VerticalSliceArchExample/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using VerticalSliceArchExample.Data;
+
+namespace VerticalSliceArchExample.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var currentAssembly = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(currentAssembly);
+
+        services.AddMediatR(currentAssembly);
+
+        services.AddValidatorsFromAssembly(currentAssembly);
+
+        services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("VerticalSliceArchDB"));
+
+        return services;
+    }
+}
